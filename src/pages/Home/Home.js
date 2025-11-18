472cba192e88fd7263dda7d418f20ac72fcbec94
Biel29Souza <biel29souza98@gmail.com>
2025-11-18 19:01:47 -0300

    Home - watch

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    
+    <div className="Home">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a className='a_styles'>Gabriel Souza</a>
+    </div>
+  );
+}
+
+export default Home;
